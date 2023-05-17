6d071b254447e9c503e39c02f7a7e085cb065a5e
Brenda Mejia <mejiab19@gmail.com>
2023-05-17 16:04:45 -0500

    GridLayout

--- TheLabApp/main.py
+++ TheLabApp/main.py
@@ -2,8 +2,12 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
 
+
+# class GridLayoutExample(GridLayout):
+#     pass
 class AnchorLayoutExample(AnchorLayout):
     pass
 class BoxLayoutExample(BoxLayout):
