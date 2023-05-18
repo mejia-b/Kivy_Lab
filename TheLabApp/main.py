85f2ca8232d68f3ef79211d559fad0dc75eff0c2
Brenda Mejia <mejiab19@gmail.com>
2023-05-17 22:35:33 -0500

    Stack Layout

--- TheLabApp/main.py
+++ TheLabApp/main.py
@@ -3,7 +3,21 @@ from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.stacklayout import StackLayout
 from kivy.uix.button import Button
+from kivy.metrics import dp
+
+class StackLayoutExample(StackLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        # self.orientation = "lr-bt"
+        for i in range(1,101):
+            #size = dp(100) + i * 10
+            size = dp(100)
+            b = Button(text=str(i),size_hint=(None,None), size=(size, size))
+            self.add_widget(b)
+        # for i in range():
+        #     pass
 
 
 # class GridLayoutExample(GridLayout):
