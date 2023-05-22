83d01dd06745da8fc36f52fa76b36de21f8d12ec
Brenda Mejia <mejiab19@gmail.com>
2023-05-21 22:41:16 -0500

    Adding vertical lines

--- GalaxyAppV1/main.py
+++ GalaxyAppV1/main.py
@@ -1,6 +1,7 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import NumericProperty
+from kivy.graphics import Color,Line
 class MainWidget(Widget):
     # create these properties in order to be able to update each x and y coordinate dynamically
     # in order to view the value of each property as the window is resized we can define a function as such:
@@ -9,10 +10,14 @@ class MainWidget(Widget):
     perspective_point_x = NumericProperty(0)
     perspective_point_y = NumericProperty(0)
     
+    V_NUM_LINES = 7
+    V_LINES_SPACING = .1 # percentage of width size
+    vertical_lines = []
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         # print(f"Initial W: {self.width} H: {self.height}")
+        self.init_vertical_lines()
 
     def on_parent(self, widget, parent):
         # print(f"On Parent W: {self.width} H: {self.height}")
@@ -24,7 +29,7 @@ class MainWidget(Widget):
         # print(f"On size W: {self.width} H: {self.height}")
         # self.perspective_point_x = self.width/2
         # self.perspective_point_y = self.height * 0.75
-        pass
+        self.update_vertical_lines()
 
     def on_perspective_point_x(self, widget, value):
         # print(f"PX: {value}")
@@ -33,6 +38,28 @@ class MainWidget(Widget):
     def on_perspective_point_y(self, widget, value):
         # print(f"PY: {value}")
         pass
+
+    def init_vertical_lines(self):
+        with self.canvas:
+            Color(1,1,1)
+            #self.line = Line(points=(100,0,100,100))
+            for i in range(0,self.V_NUM_LINES):
+                self.vertical_lines.append(Line())
+    
+    def update_vertical_lines(self):
+        # x value will be middle of the screen 
+        central_line_x = int(self.width/2)
+        spacing = self.V_LINES_SPACING * self.width
+        offset = -int(self.V_NUM_LINES/2)
+        # print(f"Screen width: {self.width}, Screen height: {self.height}")
+        # print(f"central_line: {central_line_x}")
+        # print(f"spacing: {spacing}")
+        # print(f"offset: {offset}")
+        for i in range(0,self.V_NUM_LINES):
+            line_x = int(central_line_x + offset * spacing)
+            self.vertical_lines[i].points = (line_x,0,line_x,self.height)
+            # print(f"Line {i+1} points: {self.vertical_lines[i].points}")
+            offset += 1
 
 
 class GalaxyApp(App):
