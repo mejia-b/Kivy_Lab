a7b66df5d80bb3bd5f6548ae96bda00b50347aeb
Brenda Mejia <mejiab19@gmail.com>
2023-05-31 15:57:03 -0500

    perspective transformation

--- GalaxyAppV1/main.py
+++ GalaxyAppV1/main.py
@@ -57,9 +57,31 @@ class MainWidget(Widget):
         # print(f"offset: {offset}")
         for i in range(0,self.V_NUM_LINES):
             line_x = int(central_line_x + offset * spacing)
-            self.vertical_lines[i].points = (line_x,0,line_x,self.height)
+            
+            x1, y1 = self.transform(line_x, 0)
+            x2, y2 = self.transform(line_x, self.height)
+            self.vertical_lines[i].points = (x1,y1,x2,y2)
             # print(f"Line {i+1} points: {self.vertical_lines[i].points}")
             offset += 1
+
+    def transform(self, x, y):
+        # return self.transform_2D(x, y)
+        return self.transfrom_perspective(x, y)
+    
+    def transform_2D(self, x, y):
+        return int(x), int(y)
+    
+    def transfrom_perspective(self, x, y):
+        tr_y = y * self.perspective_point_y / self.height
+        if tr_y > self.perspective_point_y:
+            tr_y = self.perspective_point_y
+
+        diff_x = x - self.perspective_point_x
+        diff_y = self.perspective_point_y - tr_y
+        proportion_y = diff_y / self.perspective_point_y
+
+        tr_x = self.perspective_point_x + diff_x * proportion_y
+        return int(tr_x), int(tr_y)
 
 
 class GalaxyApp(App):
